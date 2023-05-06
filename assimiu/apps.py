c449cb84b231b1a5435302a9465d304d47ff09cd
amane <assimium2@gamil.com>
2023-05-07 00:09:43 +0100

    f

--- assimiu/apps.py
+++ assimiu/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class HoraConfig(AppConfig):
+class AssimiuConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Hora'
+    name = 'assimiu'
